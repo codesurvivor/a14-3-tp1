6bcf18f2adf1db2b20601e828311aca780fbdf11
Franck Duriez <franck.lucien.duriez@gmail.com>
2014-02-14 17:41:44 +0100

    Begin trafic generation part of the report.

--- scripts/main.m
+++ scripts/main.m
@@ -2,11 +2,38 @@ function [] = main()
 
 %% Display statics following fifo depth and stream period.
 
+fprintf('\nDisplay statics following fifo depth and stream period.\n\n');
+
 for mode=0:4
     display_stat_for_mode(4,4,mode,100000);
 end
 
-%% 
+pause;
+
+%% Display packet statistics following stream period for LRU mode.
+
+fprintf('\nDisplay packet statistics following stream period for LRU mode.\n');
+
+figure;
+for i=1:1:5
+    dat = csvread(strcat( ...
+        '../data/noc_4_4_8_1_100000_', ...
+        int2str(2^i), '_packet_trace.data'));
+    
+    dat(:,4) = ...
+        dat(:,3) - dat(:,2);
+    
+    s = subplot(2,5,i);
+    hist(s, dat(:,4));
+    title(s, sprintf('transit time (stream period: %d)', 2^i));
+    
+    dat(:,4) = ...
+        dat(:,3) - dat(:,1);
+    
+    s = subplot(2,5,5+i);
+    hist(s, dat(:,4));
+    title(s, sprintf('life time (stream period: %d)', 2^i));
+end
 
 end
 
--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -2,7 +2,13 @@
 
 # ./bin/test/noc_test <nb_input> <nb_output> <arbiter_mode> <execution_time_ns> <stream_period>
 
-mkdir -p data
+source_dir=$PWD
+
+if [ ! -e data ]
+then
+  mkdir -p data
+fi
+
 cd data
 
 for ((k=2; k<33; k=2*k));
@@ -14,7 +20,7 @@ do
       echo "fifo depth:    $k";
       echo "arbiter mode:  $i";
       echo "stream period: $j";
-      ../bin/test/noc_test 4 4 $k $i $1 $j;
+      $source_dir/bin/test/noc_test 4 4 $k $i $1 $j;
     done
   done
 done
